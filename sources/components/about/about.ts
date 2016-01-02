9124d7936edecbf822ab8f3f8df8573bbd7ee41e
admin <admin@mac-fb-076.local>
2016-01-02 01:06:18 +0100

    typings removed

---
+++ sources/components/about/about.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+    selector: 'start',
+    templateUrl: './components/start/start.html',
+    styleUrls: ['./components/start/start.css']
+})
+export class StartComponent {}
