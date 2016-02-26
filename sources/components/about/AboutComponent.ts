ae286de334f8e2e8d4d395e0f44966227372c507
Marek Chec <mail@marekchec.com>
2016-02-26 09:25:08 +0100

    renaming

--- sources/components/about/AboutComponent.ts
+++ sources/components/about/AboutComponent.ts
@@ -1,9 +1,11 @@
-import {Component} from 'angular2/core';
+import {Component}      from 'angular2/core';
+import {SvgIcon}        from "../../directives/svgIcon/SvgIconDirective";
 
 
 @Component({
     selector: 'about',
     templateUrl: './components/about/about.html',
-    styleUrls: ['./components/about/about.css']
+    styleUrls: ['./components/about/about.css'],
+    directives: [SvgIcon]
 })
 export class AboutComponent {}
--- sources/components/about/about.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from 'angular2/core';
-
-
-@Component({
-    selector: 'about',
-    templateUrl: './components/about/about.html',
-    styleUrls: ['./components/about/about.css']
-})
-export class AboutComponent {}
